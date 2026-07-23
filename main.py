27b33256b4eaaeaa2a687afc1585abf4ad8b1ec1
MohmmedFurqaan <mmftdec@gmail.com>
2026-07-23 23:17:50 +0530

    added the title agent

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 
 # importing all the module 
 from dotenv import load_dotenv
-import os 
-from pathlib import Path
-from model.service.prompt_agent import ai_video_prompt
+
+from model.service.prompt_agent import PromptAgent
 
 
-SYSTEM_VIDEO_PROMPT = Path(
-    "prompts/script_video_prompt.md"
-).read_text(encoding='utf-8')
 
-user_prompt = str(input("Descrie about your video : "))
-response = ai_video_prompt(user_prompt, SYSTEM_VIDEO_PROMPT=SYSTEM_VIDEO_PROMPT)
-print(response, flush=True)
+
+user_prompt = str(input("Describ about your video : "))
+ai_prompt_agent = PromptAgent(user_prompt)
+title = ai_prompt_agent.ai_title_prompt()
+print(f"title generated : {title}")
